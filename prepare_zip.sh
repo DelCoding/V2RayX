c2825111fea203d5e0f5b138a2d299f6f4476dd9
root <root@vultr.guest>
2019-06-04 03:23:14 +0000

    fix

--- prepare_zip.sh
+++ prepare_zip.sh
@@ -1,10 +1,3 @@
-isbeta=$(git describe --abbrev=0 --tags | grep beta)
-if [[ "$isbeta" != "" ]] 
-then 
-    xcodebuild -project V2RayX.xcodeproj -target V2RayX -configuration Debug -s
-    cd build/Debug/
-else
-    cd build/Release/
-fi
+cd build/Release/
 zip -r V2RayX.app.zip V2RayX.app
 cd ../..
